8aa95440bf6231e311362418f4d68cb5e5d6d9d2
toshinori-m <toshi3587@gmail.com>
2023-01-08 09:29:12 +0900

    mounted, create使い方

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,11 @@
 <template>
-  <h1>My ToDo App</h1>
-  <input type="text" v-model="newTodoText" /><button @click="addTodo">追加</button>
-  <!-- 完了済みを削除するボタンを押すとチェックボックスがオンになっているToDoが削除される -->
-  <button @click="clearDoneTodos">完了済みを削除する</button>
-  <p v-if="todos.length === 0">ToDoがまだありません</p>
-  <ul v-else>
-    <li v-for="todo in todos" :key="todo">
-      <input type="checkbox" v-model="todo.isDone" /><span
-        :class="{ 'todo-done': todo.isDone }"
-        >{{ todo.text }}</span
-      >
-    </li>
-  </ul>
+  <router-view/>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      newTodoText: '',
-      todos: []
-    }
-  },
-  methods: {
-    addTodo() {
-      // フォームに文字が未入力時に追加ボタンを押しても、アラートが表示されリストに追加されない
-      if (!this.newTodoText) return alert('文字を入力してください')
-      this.todos.push({
-        isDone: false,
-        text: this.newTodoText,
-      })
-      // ToDoに追加したタイミングでフォームの文字列はクリアされる
-      this.newTodoText = ''
-    },
-    // 完了済みを削除するボタンを押すとチェックボックスがオンになっているToDoが削除される
-    clearDoneTodos() {
-      this.todos = this.todos.filter((todo) => !todo.isDone)
-    },
-  },
 }
 </script>
 
-
-<style>
-body {
-  background-color: #eee;
-}
-
-.todo-done {
-  text-decoration: line-through;
-}
+<style scoped>
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>My ToDo App</h1>
+  <HelloWorld :test="name"></HelloWorld>
+  <input type="text" v-model="newTodoText" />
+  <button @click="addTodo">追加</button>
+  <!-- 完了済みを削除するボタンを押すとチェックボックスがオンになっているToDoが削除される -->
+  <button @click="clearDoneTodos">完了済みを削除する</button>
+  <p v-if="todos.length === 0">ToDoがまだありません</p>
+  <ul v-else>
+    <li v-for="todo in todos" :key="todo">
+      <input type="checkbox" v-model="todo.isDone" /><span
+        :class="{ 'todo-done': todo.isDone }"
+        >{{ todo.text }}</span
+      >
+    </li>
+  </ul>
+  <div>現在{{hoge}}です</div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from '../components/HelloWorld.vue'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { HelloWorld },
+  data() {
+    return {
+      newTodoText: '',
+      todos: [],
+      name: 'ABC',
+      hoge:"a"
+    }
+  },
+  methods: {
+    addTodo() {
+      // フォームに文字が未入力時に追加ボタンを押しても、アラートが表示されリストに追加されない
+      if (!this.newTodoText) return alert('文字を入力してください')
+      this.todos.push({
+        isDone: false,
+        text: this.newTodoText,
+      })
+      // ToDoに追加したタイミングでフォームの文字列はクリアされる
+      this.newTodoText = ''
+    },
+    // 完了済みを削除するボタンを押すとチェックボックスがオンになっているToDoが削除される
+    clearDoneTodos() {
+      this.todos = this.todos.filter((todo) => !todo.isDone)
+    },
+  },
+  created() {
+    console.log('hoge is: ' + this.hoge)
+  },
+  mounted() {
+    this.hoge = "mounted"
   }
 }
 </script>
+
+<style>
+body {
+  background-color: #eee;
+}
+
+.todo-done {
+  text-decoration: line-through;
+}
+</style>
