6206c935e942284669ea0de3faa5c502be05ee32
toshinori-m <toshi3587@gmail.com>
2023-01-10 15:40:49 +0900

    コメント追加

--- src/components/TextAndTextArea.vue
+++ src/components/TextAndTextArea.vue
@@ -1,14 +1,15 @@
 <template>
-    <div>
-      <input type="test" v-model="text" />
-      <textarea v-model="textArea"></textarea>
-    </div>
+  <div>
+    <input type="test" v-model="text" />
+    <textarea v-model="textArea"></textarea>
+    <!-- templateにinputタグなどでv-modelで紐付けるときも、text.valueではなくtextとします。 -->
+  </div>
 </template>
 <script>
 import { ref } from 'vue'
 export default({
   setup () {
-    const text = ref('')
+    const text = ref('') //リアクティブにするためref()を使用
     const textArea = ref('')
 
     return {
--- src/components/TextPage.vue
+++ src/components/TextPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <text-and-text-area ref="texts" />
+    <text-and-text-area ref="texts" /> 
+    <!-- ref="texts"で子コンポーネントを参照できるようにref属性を付与 -->
     <button @click="testAction">test</button>
   </div>
 </template>
@@ -8,15 +9,16 @@
 <script>
 import TextAndTextArea from './TextAndTextArea.vue'
 import { onMounted, ref } from 'vue'
+// import { ref } from 'vue'
 
 export default {
   components: {
     TextAndTextArea
   },
   setup () {
-    const texts = ref(null)
+    const texts = ref(null) //setup関数の中でref(null)を代入した変数を作成。また、ref="texts"としているのでconst textsとする。
     const testAction = () => {
-      console.log(texts.value.text);
+      console.log(texts.value.text); //ref関数のため、アクセスするにはtexts.valueのようにvalueを使います。
       console.log(texts.value.textArea);
     }
     onMounted(() => {
