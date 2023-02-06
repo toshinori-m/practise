db35519641bb86a5a43e9b2c5d561a0bd5bd35de
toshinori-m <toshi3587@gmail.com>
2023-02-06 11:05:25 +0900

    railsエラー

--- back/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
--- back/vendor/bundle/ruby/2.7.0/gems/devise_token_auth-1.2.1/lib/devise_token_auth/blacklist.rb
+++
@@ -1,6 +0,0 @@
-# don't serialize tokens
-if defined? Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION
-  Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION << :tokens
-else
-  Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION << :tokens
-end
