e04866f46192ab62f651a5f553f6f5cfbafd6056
toshinori-m <toshi3587@gmail.com>
2023-02-06 10:52:25 +0900

    rails_api対応

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "training_rails_production"
+  # config.active_job.queue_name_prefix = "back_production"
 
   config.action_mailer.perform_caching = false
 
