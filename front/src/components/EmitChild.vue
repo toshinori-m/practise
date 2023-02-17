4bb9a1c31dc93e745f3068228d2a6d38eb32dd87
toshinori-m <toshi3587@gmail.com>
2023-02-17 12:40:58 +0900

    axios統一

--- front/src/components/EmitChild.vue
+++ front/src/components/EmitChild.vue
@@ -15,21 +15,17 @@ export default {
   },
   methods: {
     async onClickButton () {
-      try {
-        const res = await axios.post('http://localhost:3000/api/v1/users', {
+      axios.post('http://localhost:3000/api/v1/users', {
           content4: this.content4
-          }
-        )
+        }
+      )
+      .then (res => {
         this.$emit("onClick", this.content4);
-        console.log({ res })
-        return res
-      } catch (error) {
-        console.log({ error })
-      }
+        this.users = res.data}
+      )
+      .catch (error => console.log({ error })
+      )
     }
-    // onClickButton() {
-    //   this.$emit("onClick", this.message);
-    // }
   }
 }
 </script>
--- front/src/components/TextPage.vue
+++ front/src/components/TextPage.vue
@@ -16,23 +16,22 @@ export default {
   },
   methods: {
     async testAction () {
-      try {
-        const res = await axios.post('http://localhost:3000/api/v1/users', {
+      axios.post('http://localhost:3000/api/v1/users', {
         content2: this.$refs.texts.content2,
         // テキストボックスの入力値が参照できる
         content3: this.$refs.texts.content3
         // テキストエリアの入力値が参照できる
           }
         )
+      .then (res => {
         console.log(this.$refs.texts.content2); 
         // テキストボックスの入力値が参照できる
         console.log(this.$refs.texts.content3); 
         // テキストエリアの入力値が参照できる
-        console.log({ res })
-        return res
-      } catch (error) {
-        console.log({ error })
-      }
+        this.users = res.data}
+      )
+      .catch (error => console.log({ error })
+      )
     }
   }
 }
