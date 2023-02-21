2ddeb598a63511fae838906e392c29927eae9cca
toshinori-m <toshi3587@gmail.com>
2023-02-21 15:23:34 +0900

    rails改善

--- back/app/controllers/api/v1/users_controller.rb
+++ back/app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,39 @@
 module Api
   module V1
     class UsersController < ApplicationController
-
       def index
+        users_array = muster()
+        return render json: users_array, status: 200
+      end
+
+      def show
+        return render json: { message: '成功しました', data: @user = User.find(params[:id]) }, status: 200
+      end
+
+      def create
+        users = add_create()
+        if users.save
+          return render json: { message: '成功しました', data: users }, status: 200
+        else
+          return render json: { message: '保存出来ませんでした', errors: users.errors.messages }, status: :bad_request
+        end
+      end
+
+      def destroy
+        users = delete_specified()
+        if users.destroy
+          return render json: { message: '削除に成功しました' }, status: 200
+        else
+          return render json: { message: '削除に失敗' }, status: 400
+        end
+      end
+
+      private
+      def muster()
+        users = User.all
         users = User.order(created_at: :desc)
         users_array = users.map do |user|
-          {
-            id: user.id,
+          { id: user.id,
             content1: user.content1,
             content2: user.content2,
             content3: user.content3,
@@ -14,37 +41,14 @@ module Api
             created_at: user.created_at
           }
         end
-        render json: users_array, status: 200
       end
 
-      def show
-        render json: { message: '成功しました', data: @user = User.find(params[:id]) }, status: 200
-      end
-
-      def create
+      def add_create()
         users = User.new(content1: params[:content1], content2: params[:content2], content3: params[:content3], content4: params[:content4])
-        if users.save
-          render json: { message: '成功しました', data: users }, status: 200
-        else
-          render json: { message: '保存出来ませんでした', errors: users.errors.messages }, status: :bad_request
-        end
       end
 
-      def destroy
+      def delete_specified()
         users = User.find(params[:id])
-        if users.destroy
-          render json: { message: '削除に成功しました' }, status: 200
-        else
-          render json: { message: '削除に失敗' }, status: 400
-        end
-      end
-
-      private
-
-      def user_params
-        params
-          .require(:user)
-          .permit(:content1, :content2, :content3, :content4,)
       end
     end
   end
