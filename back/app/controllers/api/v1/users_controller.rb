a5a0d9d592a83748544d4d882382d71fcf45997c
toshinori-m <toshi3587@gmail.com>
2023-02-22 12:19:49 +0900

    rails_method

--- back/app/controllers/api/v1/users_controller.rb
+++ back/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       def index
-        users_array = muster()
+        users_array = muster
         return render json: users_array, status: 200
       end
 
@@ -11,28 +11,30 @@ module Api
       end
 
       def create
-        users = add_create()
-        if users.save
-          return render json: { message: '成功しました', data: users }, status: 200
-        else
-          return render json: { message: '保存出来ませんでした', errors: users.errors.messages }, status: :bad_request
-        end
+        users = add_user
+        return render json: { message: '成功しました', data: users }, status: 200 if users.save
+        return render json: { message: '保存出来ませんでした', errors: users.errors.messages }, status: :bad_request
       end
 
       def destroy
-        users = delete_specified()
-        if users.destroy
-          return render json: { message: '削除に成功しました' }, status: 200
-        else
-          return render json: { message: '削除に失敗' }, status: 400
-        end
+        users = delete_specified
+        return render json: { message: '削除に成功しました' }, status: 200 if users.destroy
+        return render json: { message: '削除に失敗' }, status: 400
       end
 
       private
-      def muster()
-        users = User.all
-        users = User.order(created_at: :desc)
-        users_array = users.map do |user|
+      def muster
+        User.all
+        users = sort_users
+        users_array(users)
+      end
+
+      def sort_users
+        User.order(created_at: :desc)
+      end
+
+      def users_array(users)
+        users.map do |user|
           { id: user.id,
             content1: user.content1,
             content2: user.content2,
@@ -43,12 +45,12 @@ module Api
         end
       end
 
-      def add_create()
-        users = User.new(content1: params[:content1], content2: params[:content2], content3: params[:content3], content4: params[:content4])
+      def add_user
+        User.new(content1: params[:content1], content2: params[:content2], content3: params[:content3], content4: params[:content4])
       end
 
-      def delete_specified()
-        users = User.find(params[:id])
+      def delete_specified
+        User.find(params[:id])
       end
     end
   end
