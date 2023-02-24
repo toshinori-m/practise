ec83c508dd3ea4e0289ee123550797685e06a05e
toshinori-m <toshi3587@gmail.com>
2023-02-24 17:34:39 +0900

    vue変更

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <h1>My user App</h1>
-    <input type="text" v-model="content1" />
-    <button @click="addUsers">追加</button>
-    <button @click="clearDoneUsers()">完了済みを削除する</button>
+    <input type = "text" v-model = "content1"/>
+    <button @click = "addUsers">追加</button>
+    <button @click = "clearDoneUsers">完了済みを削除する</button>
     <TextPage />
     <EmitPage />
     <p v-if="users.length === 0">userがまだありません</p>
     <ul v-else>
-      <li v-for="user in users" :key="user">
-        <input type="checkbox" v-model="user.isDone" />
-          <span :class="{ 'user-done': user.isDone }"
-          >id = {{ user.id }}</span
-        >
+      <li v-for = "user in users" :key = "user">
+        <input type = "checkbox" v-model = "user.isDone"/>
+        <span :class = "{ 'user-done': user.isDone }">
+          id = {{ user.id }}
+        </span>
         <p>content1 = {{ user.content1 }}</p>
         <p>content2 = {{ user.content2 }}</p>
         <p>content3 = {{ user.content3 }}</p>
@@ -26,7 +26,7 @@ import axios from 'axios'
 import TextPage from '../components/TextPage.vue'
 import EmitPage from '../components/EmitPage.vue'
 
-export default {
+export default{
   components: { TextPage,EmitPage },
   data() {
     return {
@@ -34,61 +34,61 @@ export default {
       content1: ''
     }
   },
-  methods: {
-    getUsers () {
+  methods:{
+    getUsers(){
       axios.get('http://localhost:3000/api/v1/users')
-      .then (res => {
+      .then(res => {
         console.log({ res })
         this.users = res.data}
       )
-      .catch (error => console.log({ error })
-      )
+      .catch(this.catch_error)
     },
-    addUsers () {
+    addUsers(){
       axios.post('http://localhost:3000/api/v1/users', {
         content1: this.content1
       })
-      .then (() => {
-        this.getUsers()
-      })
-      .catch (error => console.log({ error })
-      )
-      if (!this.content1) return alert('文字を入力してください')
+      .then(this.then_get_user)
+      .catch(this.catch_error)
+      this.alert_window()
+    },
+    alert_window(){
+      if(!this.content1)
+      return alert('文字を入力してください')
       this.users.push({
         isDone: false,
         text: this.content1,
       })
       this.content1 = ''
     },
-    clearDoneUsers (){
-      if (!this.content1 == null) return
-      // this.users = this.users.filter((user) => !user.isDone)
+    clearDoneUsers(){
+      if (!this.content1 === null) return
       const user = this.users.filter((user) => user.isDone)
-      // 配列でデータが格納されているので、要素番号を指定
       for (let i = 0; i < user.length; i++) {
         const userIsDone = user[i]
         console.log(userIsDone.id)
-        if (user.id === this.users.id){
+        if(user.id === this.users.id){
           this.deleteUsers(userIsDone.id)
         }
       }
     },
-    deleteUsers (userIsDoneId) {
+    deleteUsers(userIsDoneId){
       axios.delete('http://localhost:3000/api/v1/users/' + userIsDoneId
       )
-      .then (() => {
-        this.getUsers()
-      })
-      .catch (error => console.log({ error })
-      )
+      .then(this.then_get_user)
+      .catch(this.catch_error)
+    },
+    then_get_user(){
+      () => {this.getUsers}
+    },
+    catch_error(){
+      error => console.log({ error })
     }
   },
-  mounted() {
+  mounted(){
     this.getUsers()
   }
 }
 </script>
-
 <style>
 body {
   background-color: #eee;
