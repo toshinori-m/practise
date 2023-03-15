9c1a8ee0f8f766ae6e56561bf555ad3ffea6c728
toshinori-m <toshi3587@gmail.com>
2023-03-15 16:58:15 +0900

    演習問題１です。

---
+++ back/exercises1.sh
@@ -0,0 +1,32 @@
+# !/bin/bash
+echo -n "年齢（半角数字で入力）=> "
+read age
+echo -n "性別（man, womanで入力）=> "
+read gender
+if {test -z $gender || [ -z $age ]} || {test -z $gender && [ -z $age ]} ; then  # 空白時
+  err="exit 1" # 空白時
+  echo $err
+else 
+  if [ $age -ge 0 ] && [ $age -lt 20 ] ; then  # ageが0以上20未満
+    age_selection="Child"
+  elif [ $age -ge 20 ] && [ $age -lt 60 ] ; then  # ageが20以上60未満
+    age_selection="Adult"
+  elif [ $age -ge 60 ] ; then  # ageが60以上
+    age_selection="Elderly"
+  else
+    err="exit 1" # 誤入力
+    echo $err
+    return
+  fi
+  if test $gender = "man" ; then  # manと等しい
+    gender_selection="Man"
+  elif test $gender = "woman" ; then  # womanと等しい
+    gender_selection="Woman"
+  else
+    err="exit 1" # 誤入力
+    echo $err
+    return
+  fi
+    selection=$gender_selection:$age_selection
+    echo $selection
+fi
