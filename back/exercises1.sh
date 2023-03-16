cbe359b69539aa458e65effd811885fb58626d9e
toshinori-m <toshi3587@gmail.com>
2023-03-16 10:48:46 +0900

    修正しました。 よろしくお願いします。

--- back/exercises1.sh
+++ back/exercises1.sh
@@ -3,9 +3,8 @@ echo -n "年齢（半角数字で入力）=> "
 read age
 echo -n "性別（man, womanで入力）=> "
 read gender
-if {test -z $gender || [ -z $age ]} || {test -z $gender && [ -z $age ]} ; then  # 空白時
-  err="exit 1" # 空白時
-  echo $err
+if test -z $gender || [ -z $age ] ; then  # 空白時
+  echo "exit 1" # 空白時
 else 
   if [ $age -ge 0 ] && [ $age -lt 20 ] ; then  # ageが0以上20未満
     age_selection="Child"
@@ -14,18 +13,16 @@ else
   elif [ $age -ge 60 ] ; then  # ageが60以上
     age_selection="Elderly"
   else
-    err="exit 1" # 誤入力
-    echo $err
-    return
+    echo "exit 1" # 空白時
+    exit
   fi
   if test $gender = "man" ; then  # manと等しい
     gender_selection="Man"
   elif test $gender = "woman" ; then  # womanと等しい
     gender_selection="Woman"
   else
-    err="exit 1" # 誤入力
-    echo $err
-    return
+    echo "exit 1" # 空白時
+    exit
   fi
     selection=$gender_selection:$age_selection
     echo $selection
