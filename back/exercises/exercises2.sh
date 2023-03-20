0f5f539f4f731871739c30c5955c6201e856ba3b
toshinori-m <toshi3587@gmail.com>
2023-03-20 12:20:51 +0900

    修正しました。 よろしくお願いします。

--- back/exercises/exercises2.sh
+++ back/exercises/exercises2.sh
@@ -1,19 +1,20 @@
 # !/bin/bash
-files="*"
-fileary=()
-for filepath in $files; do
-  if [ -f $filepath ] ; then
-    fileary+=("$filepath")
-  fi
-done
+# files="*"
+# fileary=()
+# for filepath in $files; do
+#   if [ -f $filepath ] ; then
+#     fileary+=("$filepath")
+#   fi
+# done
 echo "ファイル一覧"
-for i in ${fileary[@]}; do
-  echo $i
-done
+# for i in ${fileary[@]}; do
+#   echo $i
+# done
+ls -1 #⇦この書き方だと doneから上と同じ結果になる
 read -p 'ファイル名を入力してください: ' file_name
 if [ -e $file_name ] ; then
   read -p 'ファイルに追記する値を入力してください: ' input_val
-  echo "$input_val" >>$file_name
+  echo "$input_val" >> $file_name
 else
-  echo 'ファイルが存在しません';
+  echo 'ファイルが存在しません'
 fi
