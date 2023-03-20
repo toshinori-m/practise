cde21045f09868060a140c8c094c2c9beb39df2f
toshinori-m <toshi3587@gmail.com>
2023-03-20 09:43:20 +0900

    問二です。 よろしくお願いします。

---
+++ back/exercises/exercises2.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+files="*"
+fileary=()
+for filepath in $files; do
+  if [ -f $filepath ] ; then
+    fileary+=("$filepath")
+  fi
+done
+echo "ファイル一覧"
+for i in ${fileary[@]}; do
+  echo $i
+done
+read -p 'ファイル名を入力してください: ' file_name
+if [ -e $file_name ] ; then
+  read -p 'ファイルに追記する値を入力してください: ' input_val
+  echo "$input_val" >>$file_name
+else
+  echo 'ファイルが存在しません';
+fi
